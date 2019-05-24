98c0214803d985bde21e4722f5ee9dadce2bd6c8
Ilya <ilya.shelepov2000@gmail.com>
2019-05-24 20:30:42 +0300

    Added work with masters

--- bsuir.py
+++ bsuir.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
-#%%
+
+# %%
 import requests
+from pprint import pprint
+import csv
+
 
 def get_group(group_num):
     url = 'https://journal.bsuir.by/api/v1/studentGroup/schedule'
     par = {'studentGroup': group_num}
     resp = requests.get(url, params=par)
+    if resp.text == '':
+        raise Exception('Не удалось найти расписание группы {}, \
+                        проверьте правильность введённой информации'.format(group_num))
     resp_json = resp.json()
+
     cur_week = resp_json['currentWeekNumber']
     days = resp_json['schedules']
 
@@ -17,4 +25,67 @@ def get_group(group_num):
             if cur_week in subj['weekNumber']:
                 res.setdefault(day['weekDay'], []).append(subj)
     return res
+
+
+def get_masters_update():
+    response = requests.get('https://journal.bsuir.by/api/v1/employees')
+    masters = response.json()
+    fields = ['fio', 'id']
+    with open("masters.csv", 'w', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fields)
+        writer.writeheader()
+        for master in masters:
+            fio = master['fio']
+            ind = fio.find('(')
+            if ind != -1:
+                fio = fio[:ind].rstrip(' ')
+            writer.writerow({'fio': fio, 'id': master['id']})
+
+
+def get_masters():
+    masters = []
+    with open('masters.csv', 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for master in reader:
+            masters.append(master)
+    return masters
+
+def get_master(name_to_find):
+    masters = get_masters()
+    ID = None
+    for master in masters:
+        if master['fio'] == name_to_find:
+            ID = master['id']
+            break
+    if ID is None:
+        raise Exception(
+                'Не получилось найти {}, проверьте правильность ввода'.format(name_to_find))
+    par = {'employeeId': ID}
+    response = requests.get(
+            'https://journal.bsuir.by/api/v1/portal/employeeSchedule',
+            params=par
+            )
+    resp_json = response.json()
+    cur_week = resp_json['currentWeekNumber']
+    days = resp_json['schedules']
+
+    res = {}
+
+    for day in days:
+        for subj in day['schedule']:
+            if cur_week in subj['weekNumber']:
+                res.setdefault(day['weekDay'], []).append(subj)
+    return res
+    pprint(res)
+
+
+
+
+
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -11,29 +11,14 @@ file.close()
 bot = telebot.TeleBot(TOKEN)
 session = Session()
 
-@bot.message_handler(commands=['start'])
-def start_handler(message):
-    bot.send_message(message.chat.id , 'Привет, хочешь узнать расписание?\nИспользую команду /group')
 
-@bot.message_handler(commands='group')
-def group_handler(message):
-    msg = bot.send_message(message.chat.id , 'Какой номер группы?')
-    bot.register_next_step_handler(msg, find_group)
-    session.isRunning = True
-
-def find_group(message):
-    chat_id = message.chat.id
-    text = message.text
-    if not (len(text) == 6 or text.isdigit() == True):
-        msg = bot.send_message(chat_id, 'Номер группы должен состоять из 6 цифр, введи ещё раз.')
-        bot.register_next_step_handler(msg, find_group)
-        return
-    sch = bsuir.get_group(text)
-    week = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
-    for day in week:
+def make_schedule_list_by_days(schedule):
+    result = []
+    #week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    for day in schedule.keys():
         mess = ''
         mess += day + '\n\n'
-        for subj in sch[day]:
+        for subj in schedule[day]:
             time = subj['lessonTime']
             name = subj['subject']
             typee = subj['lessonType']
@@ -42,12 +27,41 @@ def find_group(message):
                 aud = ''
             else:
                 aud = aud_list[0]
-            mess += '{} | {:<5s}({}) | {:>5s}\n'.format(time,name,typee,aud)
+            mess += '{} | {:<5s}({}) | {:>5s}\n'.format(time, name, typee, aud)
+        result.append(mess)
+    return result
 
-        bot.send_message(chat_id,mess)
+
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id , 'Привет, хочешь узнать расписание?\nИспользую команду /group')
+
+
+@bot.message_handler(commands=['group'])
+def group_handler(message):
+    msg = bot.send_message(message.chat.id , 'Какой номер группы?')
+    bot.register_next_step_handler(msg, find_group)
+    session.isRunning = True
+
+
+def find_group(message):
+    chat_id = message.chat.id
+    text = message.text
+    if not (len(text) == 6 or text.isdigit() == True):
+        msg = bot.send_message(chat_id, 'Номер группы должен состоять из 6 цифр, введи ещё раз.')
+        bot.register_next_step_handler(msg, find_group)
+        return
+    try:
+        sch = bsuir.get_group(text)
+    except Exception as e:
+        bot.send_message(chat_id, e)
+    schedule_by_days = make_schedule_list_by_days(sch)
+    for day_sch in schedule_by_days:
+        bot.send_message(chat_id, day_sch)
     session.isRunning = False
 
-@bot.message_handler(commands='master')
+
+@bot.message_handler(commands=['master'])
 def master_handler(message):
     msg = bot.send_message(message.chat.id , 'ФИО преподавателя?')
     bot.register_next_step_handler(msg, find_master)
@@ -55,7 +69,21 @@ def master_handler(message):
 def find_master(message):
     chat_id = message.chat.id
     text = message.text
+    try:
+        sch = bsuir.get_master(text)
+    except Exception as e:
+        bot.send_message(message.chat.id , e)
+        return
+    schedule_by_days = make_schedule_list_by_days(sch)
+    for day_sch in schedule_by_days:
+        bot.send_message(chat_id, day_sch)
 
-    bot.send_message(chat_id,'Здесь будет расписание препода '+text)
+
+@bot.message_handler(content_types=['text'])
+def common_text_handler(message):
+    chat_id = message.chat.id
+    text = message.text
+    bot.send_message(chat_id, 'Повторяю: '+text)
+
 
 bot.polling(none_stop=True)
